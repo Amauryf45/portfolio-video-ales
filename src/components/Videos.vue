cf369f1d93e12446bf5e60d57c0803fbfd85a952
Amauryf45 <37706836+Amauryf45@users.noreply.github.com>
2025-11-18 15:02:17 +0100

    Update Videos.vue

--- src/components/Videos.vue
+++ src/components/Videos.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+<!-- import { Button } from "@/components/ui/button"; -->
 import {
   Dialog,
   DialogContent,
